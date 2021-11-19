16f494a8b3c666298474abd5fdcabf76c272b2c0
blakeyi <blakeyi@tencent.com>
2021-11-19 15:01:46 +0800

    feature:增加mermaid甘特图

--- src/lib/core/code-plugin.js
+++ src/lib/core/code-plugin.js
@@ -19,6 +19,20 @@ module.exports = function flow (md) {
         str = `<div class="md-flowchart" ${' data-source="' + parseInt(token.map[0] + 1) + '"'} style="` + 'width:' + temps[1] + ';height:' + temps[2] + `">${code}</div>`
       }
       return str
+    } else if (token.info.startsWith('mermaid')) {
+      const temps = token.info.split(/\s+/)
+      let str = ''
+      if (temps.length === 1) {
+        str = `<div class="md-mermaid" ${' data-source="' + parseInt(token.map[0] + 1) + '"'}>${code}</div>`
+      } else if (temps.length === 2) {
+        temps[1] = temps[1].indexOf('px') !== -1 ? temps[1] : (temps[1] + '%')
+        str = `<div class="md-mermaid" ${' data-source="' + parseInt(token.map[0] + 1) + '"'} style="` + 'width:' + temps[1] + `">${code}</div>`
+      } else if (temps.length >= 3) {
+        temps[1] = temps[1].indexOf('px') !== -1 ? temps[1] : (temps[1] + '%')
+        temps[2] = temps[2].indexOf('px') !== -1 ? temps[2] : (temps[2] + '%')
+        str = `<div class="md-mermaid" ${' data-source="' + parseInt(token.map[0] + 1) + '"'} style="` + 'width:' + temps[1] + ';height:' + temps[2] + `">${code}</div>`
+      }
+      return str  
     } else {
       const lang = token.info
       if (lang && hljs.getLanguage(lang)) {
